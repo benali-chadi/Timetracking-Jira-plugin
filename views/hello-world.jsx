6d806cf730f83ea74555863c10a9d8a47bad1d42
Ziad GHOUZLANI <zghouzlani@leyton.com>
2023-07-26 18:02:00 +0100

    improvements

--- views/hello-world.jsx
+++ views/hello-world.jsx
@@ -1,109 +1,133 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Button from "@atlaskit/button";
 import TableTree, {
-  Cell,
-  Header,
-  Headers,
-  Row,
-  Rows,
+    Cell,
+    Header,
+    Headers,
+    Row,
+    Rows,
 } from "@atlaskit/table-tree";
 
 export default function HelloWorld() {
-  const [issues, setIssues] = useState([]);
-  const [worklogMap, setWorklogMap] = useState(new Map());
-  const [displayItems, setDisplayItems] = useState([]);
+    const [issues, setIssues] = useState([]);
+    const [issueTypes, setIssueTypes] = useState([])
+    const [worklogMap, setWorklogMap] = useState(new Map());
+    const [displayItems, setDisplayItems] = useState([]);
 
-  useEffect(() => {
-    AP.request({
-      url: "/rest/api/3/search?jql=",
-      type: "GET",
-      success: (data) => {
-        const parsed = JSON.parse(data);
-        setIssues(parsed.issues);
-        parsed.issues.forEach((element) => {
-          AP.request({
-            url: `/rest/api/3/issue/${element.key}/worklog`,
+    useEffect(() => {
+        AP.request({
+            url: "/rest/api/3/search?jql=",
             type: "GET",
             success: (data) => {
-              const parsedWorklog = JSON.parse(data);
-
-              setWorklogMap((prev) => {
-                let newMap = new Map(prev);
-                newMap.set(element.key, parsedWorklog);
-                return newMap;
-              });
+                const parsed = JSON.parse(data);
+                setIssues(parsed.issues);
+                setIssueTypes(parsed.issues.map((e) => {
+                    return {
+                        issueId: e.key,
+                        issueType: {
+                            name: e.fields.issuetype.name,
+                            iconUrl: e.fields.issuetype.iconUrl
+                        }
+                    }
+                }))
+                parsed.issues.forEach((element) => {
+                    AP.request({
+                        url: `/rest/api/3/issue/${element.key}/worklog`,
+                        type: "GET",
+                        success: (data) => {
+                            const parsedWorklog = JSON.parse(data);
+                            // worklogMap.set(element.key,{nam})
+                            setWorklogMap((prev) => {
+                                let newMap = new Map(prev);
+                                newMap.set(element.key, parsedWorklog);
+                                return newMap;
+                            });
+                        },
+                        error: (err) => {
+                            console.log("err:", err);
+                        },
+                    });
+                });
             },
             error: (err) => {
-              console.log("err:", err);
+                console.log("err:", err);
             },
-          });
         });
-      },
-      error: (err) => {
-        console.log("err:", err);
-      },
-    });
-  }, []);
+    }, []);
 
-  useEffect(() => {
-    if (worklogMap.size > 0) {
-      setDisplayItems(getItems());
-    }
-  }, [worklogMap]);
+    useEffect(() => {
+        if (worklogMap.size > 0) {
+            setDisplayItems(getItems());
+        }
+    }, [worklogMap]);
 
-  const getItems = () => {
-    let arr = Array.from(worklogMap).map(([k, v]) => {
-      console.log(v);
-      return {
-        id: k,
-        title: k,
-        description: v.total.toString(),
-        children: v.worklogs.map((elm) => {
-          return {
-            id: elm.id,
-            title: elm.author.displayName,
-            description: elm.timeSpent,
-            startDate: new Date(elm.started).toLocaleString(),
-          };
-        }),
-      };
-    });
-    return arr;
-  };
+    const getItems = () => {
+        let arr = Array.from(worklogMap).map(([k, v]) => {
+            console.log(v);
+            return {
+                id: k,
+                title: k,
+                description: v.total.toString(),
+                children: v.worklogs.map((elm) => {
+                    return {
+                        id: elm.id,
+                        title: elm.author.displayName,
+                        description: elm.timeSpent,
+                        startDate: new Date(elm.started).toLocaleString(),
+                    };
+                }),
+            };
+        });
+        return arr;
+    };
 
-  return (
-    <>
-      <Button
-        appearance="primary"
-        onClick={() =>
-          AP.dialog.create({
-            key: "dialog",
-            chrome: false,
-          })}
-      >
-        Create Worklog
-      </Button>
-      <TableTree>
-        <Headers>
-          <Header width={120}>Users</Header>
-          <Header width={300}>Time Spent</Header>
-          <Header width={300}>Start Date</Header>
-        </Headers>
-        <Rows
-          items={displayItems}
-          render={({ id, title, description, startDate, children = [] }) => (
-            <Row
-              itemId={id}
-              items={children}
-              hasChildren={children.length > 0}
+    return (
+        <>
+            <Button
+                appearance="primary"
+                onClick={() =>
+                    AP.dialog.create({
+                        key: "dialog",
+                        chrome: false,
+                    })}
             >
-              <Cell>{title}</Cell>
-              <Cell>{description}</Cell>
-              <Cell>{startDate}</Cell>
-            </Row>
-          )}
-        />
-      </TableTree>
-    </>
-  );
+                Create Worklog
+            </Button>
+            <TableTree>
+                <Headers>
+                    <Header width={300}>Issues</Header>
+                    <Header width={300}>Time Spent</Header>
+                    <Header width={300}>Start Date</Header>
+                </Headers>
+                <Rows
+                    items={displayItems}
+                    render={({id, title, description, startDate, children = []}) => (
+                        <Row
+                            itemId={id}
+                            items={children}
+                            hasChildren={children.length > 0}
+                        >
+                            <Cell>
+                                {issueTypes.map(e => {
+                                    if (e.issueId === title) {
+                                        console.log("*********************\n",title)
+                                        return (
+                                            <span key={e.issueId}>
+                                                <img
+                                                    src={`${e.issueType.iconUrl}`}
+                                                    alt="Issue Icon"/>
+                                              </span>
+                                        );
+                                    }
+                                })}
+                                {title}
+                            </Cell>
+                            <Cell>{description}</Cell>
+                            <Cell>{startDate}</Cell>
+                        </Row>
+                    )}
+                />
+            </TableTree>
+        </>
+    );
 }
--- views/web-item.jsx
+++ views/web-item.jsx
@@ -1,167 +1,183 @@
 import Button from "@atlaskit/button";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
 import Modal, {
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
-  ModalTitle,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+    ModalTitle,
 } from "@atlaskit/modal-dialog";
 import Select from "@atlaskit/select";
 import TextField from "@atlaskit/textfield";
 import TextArea from "@atlaskit/textarea";
-import { DateTimePicker } from "@atlaskit/datetime-picker";
+import {DateTimePicker} from "@atlaskit/datetime-picker";
 
 export default function WebItem() {
-  const [issues, setIssues] = useState([]);
-  const [selectedIssue, setSelectedIssue] = useState(null);
-  const [timeSpent, setTimeSpent] = useState(0);
-  const [comment, setComment] = useState("");
-  const [startDate, setStartDate] = useState(null);
-  const [isStartDate, setIsStartDate] = useState(true);
+    const [issues, setIssues] = useState([]);
+    const [selectedIssue, setSelectedIssue] = useState(null);
+    const [timeSpent, setTimeSpent] = useState(0);
+    const [comment, setComment] = useState("");
+    const [startDate, setStartDate] = useState(null);
+    const [isStartDate, setIsStartDate] = useState(true);
+    const [sysDate, setSysDate] = useState(new Date().toISOString().slice(0, 16) + "+0100")
+    const [contextIssue, setContextIssue] = useState([])
 
-  const onClose = () => {
-    AP.dialog.close();
-  };
+    const [test, setTest] = useState({label: 'REC-1', value: '10008'})
 
-  useEffect(() => {
-    AP.request({
-      url: "/rest/api/3/search?jql=",
-      type: "GET",
-      success: (data) => {
-        const parsed = JSON.parse(data);
-
-        const iss = parsed.issues.map((i) => {
-          return {
-            label: i.key,
-            value: i.id,
-          };
-        });
-
-        setIssues(iss);
-      },
-      error: (err) => {
-        console.log("err:", err);
-      },
-    });
-  }, []);
-
-  useEffect(() => {
-    if (!startDate) return;
-    else setIsStartDate(true);
-  }, [startDate]);
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (!startDate) {
-      setIsStartDate(false);
-      return;
-    }
-    const splitedStartDate = startDate.split("+");
-    const dateStart = splitedStartDate[0] + ":00.000+" + splitedStartDate[1];
-    console.log("startDate", dateStart);
-
-    const bodyData = {
-      comment: {
-        content: [
-          {
-            content: [
-              {
-                text: comment,
-                type: "text",
-              },
-            ],
-            type: "paragraph",
-          },
-        ],
-        type: "doc",
-        version: 1,
-      },
-      started: dateStart,
-      timeSpentSeconds: timeSpent * 60 * 60,
+    const onClose = () => {
+        AP.dialog.close();
     };
 
-    AP.request({
-      url: `/rest/api/3/issue/${selectedIssue}/worklog`,
-      type: "POST",
-      data: JSON.stringify(bodyData),
-      contentType: "application/json",
-      success: (res) => {
-        console.log("Created Worklog:", res);
-      },
-      error: (err) => {
-        console.log("err", err);
-      },
-    });
-    AP.dialog.close();
-  };
+    useEffect(() => {
+        AP.context.getContext(function (response) {
+            const obj = [{
+                label: response.jira.issue.key,
+                value: response.jira.issue.id
+            }]
+            console.log("Response : ",response)
+            console.log("Object : ",obj)
+            setContextIssue(obj)
+        });
 
-  return (
-    <Modal onClose={onClose}>
-      <form
-        onSubmit={onSubmit}
-        style={{
-          height: "100%",
-          width: "100%",
-        }}
-      >
-        <ModalHeader>
-          <ModalTitle>Create worklog</ModalTitle>
-        </ModalHeader>
-        <ModalBody
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "1rem",
-          }}
-        >
-          <Select
-            required
-            placeholder="Select an issue"
-            options={issues}
-            onChange={(v) => {
-              setSelectedIssue(v.value);
-            }}
-          />
+        AP.request({
+            url: "/rest/api/3/search?jql=",
+            type: "GET",
+            success: (data) => {
+                const parsed = JSON.parse(data);
+                const iss = parsed.issues.map((i) => {
+                    return {
+                        label: i.key,
+                        value: i.id,
+                    };
+                });
+                setIssues(iss);
+            },
+            error: (err) => {
+                console.log("err:", err);
+            },
+        });
 
-          <TextField
-            type="number"
-            max={8}
-            min={1}
-            placeholder="Enter time spent in hours"
-            isRequired
-            onChange={(e) => {
-              return setTimeSpent(e.target.value);
-            }}
-          />
+    }, []);
 
-          <TextArea
-            style={{ marginTop: "10px" }}
-            placeholder="Enter a comment"
-            resize="auto"
-            onChange={(e) => setComment(e.target.value)}
-          />
+    useEffect(() => {
+        if (!startDate) return;
+        else setIsStartDate(true);
+    }, [startDate]);
 
-          <div>
-            <label htmlFor="Start Date">Start Date</label>
-            <DateTimePicker
-              name="Start Date"
-              timePickerProps={{}}
-              onChange={(e) => {
-                console.log(e);
-                return setStartDate(e);
-              }}
-            />
-            {!isStartDate && (
-              <p style={{ color: "red" }}>Please select a start date</p>
-            )}
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <Button appearance="primary" type="submit">Create</Button>
-          <Button onClick={onClose}>Cancel</Button>
-        </ModalFooter>
-      </form>
-    </Modal>
-  );
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (!startDate) {
+            setIsStartDate(false);
+            return;
+        }
+        const splitedStartDate = startDate.split("+");
+        const dateStart = splitedStartDate[0] + ":00.000+" + splitedStartDate[1];
+        console.log("startDate", dateStart);
+
+        const bodyData = {
+            comment: {
+                content: [
+                    {
+                        content: [
+                            {
+                                text: comment,
+                                type: "text",
+                            },
+                        ],
+                        type: "paragraph",
+                    },
+                ],
+                type: "doc",
+                version: 1,
+            },
+            started: dateStart,
+            timeSpentSeconds: timeSpent * 60 * 60,
+        };
+
+        AP.request({
+            url: `/rest/api/3/issue/${selectedIssue}/worklog`,
+            type: "POST",
+            data: JSON.stringify(bodyData),
+            contentType: "application/json",
+            success: (res) => {
+                console.log("Created Worklog:", res);
+            },
+            error: (err) => {
+                console.log("err", err);
+            },
+        });
+        AP.dialog.close();
+    };
+
+    return (
+        <Modal onClose={onClose}>
+            <form
+                onSubmit={onSubmit}
+                style={{
+                    height: "100%",
+                    width: "100%",
+                }}
+            >
+                <ModalHeader>
+                    <ModalTitle>Create worklog</ModalTitle>
+                </ModalHeader>
+                <ModalBody
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "1rem",
+                    }}
+                >
+                    <Select
+                        required
+                        placeholder={`Select an issue `}
+                        options={issues}
+                        defaultValue={issues[0]}
+                        // onChange={(v) => {
+                        //     setSelectedIssue(v.value);
+                        // }}
+                    />
+
+
+                    <TextField
+                        type="number"
+                        max={8}
+                        min={1}
+                        placeholder="Enter time spent in hours"
+                        isRequired
+                        onChange={(e) => {
+                            return setTimeSpent(e.target.value);
+                        }}
+                    />
+
+                    <TextArea
+                        style={{marginTop: "10px"}}
+                        placeholder="Enter a comment"
+                        resize="auto"
+                        onChange={(e) => setComment(e.target.value)}
+                    />
+
+                    <div>
+                        <label htmlFor="Start Date">Start Date</label>
+                        <DateTimePicker
+                            name="Start Date"
+                            defaultValue={sysDate}
+                            timePickerProps={{}}
+                            onChange={(e) => {
+                                // console.log(e);
+                                return setStartDate(e);
+                            }}
+                        />
+                        {!isStartDate && (
+                            <p style={{color: "red"}}>Please select a start date</p>
+                        )}
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <Button appearance="primary" type="submit">Create</Button>
+                    <Button onClick={onClose}>Cancel</Button>
+                </ModalFooter>
+            </form>
+        </Modal>
+    );
 }
